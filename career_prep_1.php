8055b21e51e901a379be328427d7cb7b938aaadb
Gopika Krishnan <22mp1959@rit.ac.in>
2023-10-19 15:11:07 +0530

    18/10/2023

---
+++ career_prep_1.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/dash-style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
+    <title></title>
+</head>
+<body>
+<div class="top-bar">
+        <a href="index.php"><i class="fa fa-arrow-left back-button" aria-hidden="true"></i></a>
+        <h5><a href="career_prep_1.php">
+                <i class="fa fa-th-large" aria-hidden="true"></i>
+                <span class="heading">Career Prep Toolkit</span>
+            </a></h5>
+    </div>
+    <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+<div class="container">
+        <nav>
+            <ul class="list-items">
+                <li><a href="career_prep_1.php" class="nav-links active">
+                <i class="fa-solid fa-highlighter"></i>
+                    <span class="nav-item">Mock test</span>
+                </a></li>
+                <li><a href="career_prep_2.php" class="nav-links">
+                <i class="fa-solid fa-video"></i>
+                    <span class="nav-item">Mock Interviews</span>
+                </a></li>
+                <li><a href="career_prep_3.php" class="nav-links">
+                <i class="fa-solid fa-pen-to-square"></i>
+                    <span class="nav-item">Live exams</span>
+                </a></li>
+                <li><a href="career_prep_4.php" class="nav-links">
+                <i class="fa-solid fa-book-open"></i>
+                    <span class="nav-item">Study Meterials</span>
+                </a></li>
+                <li><a href="career_prep_5.php" class="nav-links">
+                <i class="fa-solid fa-link"></i>
+                    <span class="nav-item">Usefull links</span>
+                </a></li>
+            </ul>
+        </nav>
+        <main>
+        
+     
+            
+            <div class="card-category-1">
+                
+                <div class="basic-card basic-card-aqua">
+                    <div class="card-content">
+                        <span class="card-title">Aptitude</span>
+                        <p class="card-text">
+                            Aptitude
+                        </p>
+                    </div>
+    
+                    <div class="card-link">
+                        <a href="aptitude-exam.php" title="Read Full"><span>Read Full</span></a>
+                    </div>
+                </div>
+    
+                <div class="basic-card basic-card-lips">
+                    <div class="card-content">
+                        <span class="card-title">Coding</span>
+                        <p class="card-text">
+                            Coding
+                        </p>
+                    </div>
+    
+                    <div class="card-link">
+                        <a href="#" title="Read Full"><span>Read Full</span></a>
+                    </div>
+                </div>
+    
+                
+                <div class="basic-card basic-card-dark">
+                    <div class="card-content">
+                        <span class="card-title">English</span>
+                        <p class="card-text">
+                        English
+                        </p>
+                    </div>
+    
+                    <div class="card-link">
+                        <a href="english-exam.php" title="Read Full"><span>Read Full</span></a>
+                    </div>
+                </div>
+                
+            </div>
+
+        </main>
+    </div>
+</body>
+</html>
